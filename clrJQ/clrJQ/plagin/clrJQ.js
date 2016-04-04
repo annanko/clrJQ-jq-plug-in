b3fc09cda516ad3589574aa78bbad76bd4dee2d5
annanko <annanko@hotmail.com>
2016-04-04 13:00:11 +0200

    backup files

---
+++ clrJQ/clrJQ/plagin/clrJQ.js
@@ -0,0 +1,109 @@
+/*
+* clrJQ.js - JQuery plugin
+* lrJQ is JQuery mini-plugin that set positions to text,images and blocks.
+*
+* Name:			clrJQ.js
+* Author:		Andrej Nankov - http://andrej.uk.to/
+* Date:			july 23, 2015		
+* Version:		1.0
+* Example:		/
+*	
+*/
+
+(function( $ ) {
+	
+	
+$.fn.centerImg = function()
+{
+	$(this).css("display", "block");
+	$(this).css( { marginLeft : "auto", marginRight : "auto" } );
+	return this;
+}
+$.fn.centerImg = function(w,h)
+{
+	$(this).centerImg();
+	$(this).innerWidth(w);
+	$(this).innerHeight(h);
+	return this;
+}
+$.fn.rightImg = function()
+{
+	$(this).css("float", "right");
+	$(this).css("clear", "both"); 
+	return this;
+}
+$.fn.rightImg = function(w,h)
+{
+	$(this).rightImg();
+	$(this).innerWidth(w);
+	$(this).innerHeight(h);
+	return this;
+}
+$.fn.leftImg = function()
+{
+	$(this).css("float", "left");
+	$(this).css("clear", "both"); 
+	return this;
+}
+$.fn.leftImg = function(w,h)
+{
+	$(this).leftImg();
+	$(this).innerWidth(w);
+	$(this).innerHeight(h);
+	return this;
+}
+$.fn.centerBlock = function(w)
+{
+	$(this).centerImg();
+	$(this).innerWidth(w);	
+	return this;
+}
+$.fn.centerBlock = function(w,h)
+{
+	$(this).centerImg();
+	$(this).innerWidth(w);
+	$(this).innerHeight(h);
+	return this;	
+}
+$.fn.rightBlock = function(w)
+{
+	$(this).rightImg();
+	$(this).innerWidth(w);
+	return this;
+}
+$.fn.rightBlock = function(w,h)
+{
+	$(this).rightImg();
+	$(this).innerWidth(w);
+	$(this).innerHeight(h);
+	return this;
+}
+$.fn.leftBlock = function(w)
+{
+	$(this).leftImg();
+	$(this).innerWidth(w);
+	return this;
+}
+$.fn.leftBlock = function(w,h)
+{
+	$(this).leftImg();
+	$(this).innerWidth(w);
+	$(this).innerHeight(h);
+	return this;
+}
+$.fn.centerText = function(w)
+{
+		$(this).css("text-align", "center");
+		return this;
+}
+$.fn.leftText = function(w)
+{
+		$(this).css("text-align", "left");
+		return this;
+}
+$.fn.rightText = function(w)
+{
+		$(this).css("text-align", "right");
+		return this;
+}
+}( jQuery ));
